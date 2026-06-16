b820e46a71472be59a635763b53b72d9926b7b09
sina <sinamatari23@gmail.com>
2026-06-16 16:26:50 +0330

    add swagger docs with drf-yasg

--- todo_project/todo_project/urls.py
+++ todo_project/todo_project/urls.py
@@ -1,10 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Todo API",
+        default_version='v1',
+        description="مستندات API پروژه Todo",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lambda req: redirect('task-list')),
     path('', include('tasks.urls')),
     path('tasks/api/v1/', include('tasks.api.v1.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
 ]
